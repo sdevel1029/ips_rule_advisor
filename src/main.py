9e1e8741468f81c560b7a4dc851bedeb9e5d41b1
DESKTOP-1KU4MK0\gkok1 <sdevel1029@gmail.com>
2024-08-19 14:20:05 +0900

    정보수집 라우터 + 서비스 추가 js 로 json 형태 요청 받아서 콘솔에 출력되게끔 까지 구현 + html 수정

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from fastapi.staticfiles import StaticFiles
 from src.auth.auth_routes import router as auth_router
 from src.openai.openai_routes import router as openai_router
 from src.root.root import router as root_router
+from src.getinfo.getinfo_routes import router as getinfo_router
 import uvicorn
 
 app = FastAPI()
@@ -14,6 +15,7 @@ app.mount("/static", StaticFiles(directory="src/static"), name="static")
 app.include_router(auth_router, prefix="/auth")
 app.include_router(openai_router, prefix="/openai", tags=["openai"])
 app.include_router(root_router)  
+app.include_router(getinfo_router)
 
 if __name__ == "__main__":
     uvicorn.run("src.main:app", host="127.0.0.1", port=8000, reload=True)
