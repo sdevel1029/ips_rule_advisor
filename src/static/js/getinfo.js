4046ca9c229190b8c281b4be0a1704ba4a1b3ced
DESKTOP-1KU4MK0\gkok1 <sdevel1029@gmail.com>
2024-08-20 17:59:17 +0900

    Revert "임시"
    This reverts commit 3bdc869e9560b8396f952e27eac91b39fbe567ac.

--- src/static/js/getinfo.js
+++ src/static/js/getinfo.js
@@ -1,29 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('cve-form').addEventListener('submit', async function(event) {
-      event.preventDefault();
-
+    document.getElementById('cve-form').addEventListener('submit', async function(event) {
+      event.preventDefault();  
+  
       const cveCode = document.getElementById('cve_code').value;
-
       try {
-          const response = await fetch('/getinfo', {
-              method: 'POST',
-              headers: {
-                  'Content-Type': 'application/json'  // JSON 요청
-              },
-              body: JSON.stringify({ cve_code: cveCode })  // JSON 형식으로 데이터 전송
-          });
+        const response = await fetch('/getinfo', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ cve_code: cveCode })
+        });
+  
+        if (!response.ok) {
+          throw new Error('서버 오류: ' + response.statusText);
+        }
+  
+        const result = await response.json();
+        console.log(result);  
 
-          if (!response.ok) {
-              throw new Error('서버 오류: ' + response.statusText);
-          }
-
-          const result = await response.json();
-          console.log(result);
-
-          // 결과 페이지로 이동 코드 또는 데이터 처리
-          window.location.href = `getinfo_result.html?cve_code=${encodeURIComponent(result.cve_code)}&info=${encodeURIComponent(result.info)}`;
       } catch (error) {
-          console.error('문제 발생:', error);
+        console.error('문제 발생:', error);
+
       }
+    });
   });
-});
+  
