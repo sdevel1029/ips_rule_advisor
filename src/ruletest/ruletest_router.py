1a7289b066ee9126c60f4918fc27b9fcf3cef043
jhnoh1 <141809365+jhnoh1@users.noreply.github.com>
2024-08-30 13:15:17 +0900

    Add files via upload

--- src/ruletest/ruletest_router.py
+++ src/ruletest/ruletest_router.py
@@ -1,10 +1,12 @@
-'''import RPATools
+from src.getinfo import rpatools
 from fastapi import File, UploadFile,FastAPI,Request,Response
 from pydantic import BaseModel
 import os
 from src.database.supabase_client import get_supabase_client
 from fastapi import APIRouter, Depends, Response, Request, HTTPException
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import JSONResponse
+
 
 router = APIRouter()
 templates = Jinja2Templates(directory="src/templates")
@@ -19,100 +21,96 @@ class cve(BaseModel):
 
 
 @router.post("/uploadfile/normal")
-async def upload_file(request:Request,file: UploadFile = File(...),client=Depends(get_supabase_client)):
+async def upload_file(cve:str,request:Request,file: UploadFile = File(...),client=Depends(get_supabase_client)):
     user = request.cookies.get("user")
     user_info = client.auth.get_user(user)
-    user_id = user_info["user"]["user"]["id"]
-    folder_path = "uploaded_files/"+user_id+"/"+"normal"
+    user_id = user_info.user.id
+    folder_path = "normal_pcap/"+"/"+user_id
     os.makedirs(folder_path, exist_ok=True)
-    file_location = folder_path+"/"+file.filename
+    file_location = folder_path+"/"+cve+".pcap"
     with open(file_location, "wb") as buffer:
         buffer.write(await file.read())
     return {"info": f"file '{file.filename}' saved at '{file_location}'"}
 
 @router.post("/uploadfile/attack")
-async def upload_file(request:Request,file: UploadFile = File(...),client=Depends(get_supabase_client)):
+async def upload_file(cve:str,request:Request,file: UploadFile = File(...),client=Depends(get_supabase_client)):
     user = request.cookies.get("user")
     user_info = client.auth.get_user(user)
-    user_id = user_info["user"]["user"]["id"]
-    folder_path = "uploaded_files/"+user_id+"/"+"attack"
+    user_id = user_info.user.id
+    folder_path = "attack_pcap/"+"/"+user_id
     os.makedirs(folder_path, exist_ok=True)
-    file_location = folder_path+"/"+file.filename
+    file_location = folder_path+"/"+cve+".pcap"
     with open(file_location, "wb") as buffer:
         buffer.write(await file.read())
     return {"info": f"file '{file.filename}' saved at '{file_location}'"}
 
 
 @router.post("/test/input")
-async def test(cve:cve,request:Request,response: Response,file: UploadFile = File(...),client=Depends(get_supabase_client)):
+async def test(request:Request,response: Response,client=Depends(get_supabase_client)):
+    data = await request.json()
     user = request.cookies.get("user")
     user_info = client.auth.get_user(user)
-    user_id = user_info["user"]["user"]["id"]
-    if cve.accuracy_test and cve.accuracy_test:
+    user_id = str(user_info.user.id)
+    accuracy_test = data.get("accuracy_test", "")
+    performance_test = data.get("performance_test", "")
+    cve = str(data.get("cve", ""))
+    envi = int(data.get("envi", ""))
+    rule = str(data.get("rule", ""))
+    if accuracy_test and performance_test:
         whattest = 2
-    elif cve.accuracy_test:
+    elif accuracy_test:
         whattest = 0
     else:
         whattest = 1
-
-    filename = cve.cve+"_"+user_id
     res = (
-    client.table("testresult")
-    .insert({"rule": cve.rule, "CVEnum": cve.cve,"envi": cve.envi,"whattest": whattest,"userid": user_id})
+    client.table("testresult_duplicate")
+    .insert({"rule": rule, "CVEnum": cve,"envi": envi,"whattest": whattest,"userid": user_id})
     .execute()
 )
-    inserted_testid = res.data[0]['testid']
-    response.set_cookie(key="test", value=inserted_testid)
-    return {"status": "success"}
-    
-
-@router.post("/test/result")
-async def test(request:Request,client=Depends(get_supabase_client)):
-    test = request.cookies.get("test")
-    res = client.table("testresult").select("*").is_("testid", test).execute()
-    user = request.cookies.get("user")
-    user_info = client.auth.get_user(user)
-    user_id = user_info["user"]["user"]["id"]
-    if os.path.isfile("uploaded_files/"+user_id+"/"+"normal"):
-        if os.path.isfile("uploaded_files/"+user_id+"/"+"attack"):  #normal+attack
-            test_result = RPATools.test(
-	        cve = res["data"][0]["CVEnum"], 
-            rule= res["data"][0]["rule"], 
-	        envi = res["data"][0]["envi"], 
-	        normal_pcap_file_path  = "uploaded_files/"+user_id+"/"+"normal",
-	        attack_pacp_file_path  = "uploaded_files/"+user_id+"/"+"attack",
-	        what_test = res["data"][0]["whattest"]
+    '''
+    if os.path.isfile("normal_pcap/"+"/"+user_id+"/"+cve+".pcap"):
+        if os.path.isfile("attack_pcap/"+"/"+user_id+"/"+cve+".pcap"):  #normal+attack
+            test_result = rpatools.test(
+	        cve = cve, 
+            rule= rule, 
+	        envi = envi, 
+	        normal_pcap_file_path  = "normal_pcap/"+"/"+user_id+"/"+cve+".pcap",
+	        attack_pacp_file_path  = "attack_pcap/"+"/"+user_id+"/"+cve+".pcap",
+	        what_test = whattest 
 	        )
         else:   #only normal
-            test_result = RPATools.test(
-	        cve = res["data"][0]["CVEnum"], 
-            rule= res["data"][0]["rule"], 
-	        envi = res["data"][0]["envi"], 
-	        normal_pcap_file_path  = "uploaded_files/"+user_id+"/"+"normal",
+            test_result = rpatools.test(
+	        cve = cve, 
+            rule= rule, 
+	        envi = envi, 
+	        normal_pcap_file_path  = "normal_pcap/"+"/"+user_id+"/"+cve+".pcap",
 	        attack_pacp_file_path  = "original/attack",
-	        what_test = res["data"][0]["whattest"]
+	        what_test = whattest 
 	        )
     else: 
-        if os.path.isfile("uploaded_files/"+user_id+"/"+"attack"): #only attack
-            test_result = RPATools.test(
-	        cve = res["data"][0]["CVEnum"], 
-            rule= res["data"][0]["rule"], 
-	        envi = res["data"][0]["envi"], 
+        if os.path.isfile("attack_pcap/"+"/"+user_id+"/"+cve+".pcap"): #only attack
+            test_result = rpatools.test(
+	        cve = cve, 
+            rule= rule, 
+	        envi = envi,  
 	        normal_pcap_file_path  = "original/normal",
-	        attack_pacp_file_path  = "uploaded_files/"+user_id+"/"+"attack",
-	        what_test = res["data"][0]["whattest"]
+	        attack_pacp_file_path  = "attack_pcap/"+"/"+user_id+"/"+cve+".pcap",
+	        what_test = whattest 
 	        )
         else:  #none
-            test_result = RPATools.test(
-	        cve = res["data"][0]["CVEnum"], 
-            rule= res["data"][0]["rule"], 
-	        envi = res["data"][0]["envi"], 
+            test_result = rpatools.test(
+	        cve = cve, 
+            rule= rule, 
+	        envi = envi, 
 	        normal_pcap_file_path  = "original/normal",
 	        attack_pacp_file_path  = "original/attack",
-	        what_test = res["data"][0]["whattest"]
-	        )
-    response = (
-    client.table("countries")
+	        what_test = whattest 
+	        )'''
+    inserted_testid = res.data[0]['testid']
+    print(inserted_testid)
+    '''
+    res1 = (
+    client.table("testresult")
     .update({"normalnum": test_result["정상 패킷 결과"]["정상 패킷 수"],
              "normalfalse": test_result["정상 패킷 결과"]["오탐된 패킷 수"]
              ,"normalfalsepac": test_result["정상 패킷 결과"]["오탐된 패킷"]
@@ -123,10 +121,63 @@ async def test(request:Request,client=Depends(get_supabase_client)):
              ,"attackfalsepac": test_result["공격 패킷 결과"]["미탐된 패킷"]
              ,"attacktiembefore": test_result["공격 패킷 결과"]["평균 시간_룰 적용 전"]
              ,"attacktiemafter": test_result["공격 패킷 결과"]["평균 시간_룰 적용 후"]})
-    .eq("testid", test)
+    .eq("testid", inserted_testid)
+    .execute()
+)'''
+
+    res1 = (
+    client.table("testresult_duplicate")
+    .update({"normalnum": 123,
+             "normalfalse": 123
+             ,"normalfalsepac": 12
+             ,"normaltimebefore": 12
+             ,"normaltimeafter": 12
+             ,"attacknum": 12
+             ,"attackfalse": 12
+             ,"attackfalsepac": 12
+             ,"attacktiembefore": 12
+             ,"attacktiemafter": 12})
+    .eq("testid", inserted_testid)
     .execute()
 )
-    return test_result
+
+    response.set_cookie(key="test", value=inserted_testid)
+    return {"testid": inserted_testid}
+
+
+@router.post("/test/show")
+async def test(request:Request,response: Response,client=Depends(get_supabase_client)):
+    body = await request.json()  # Get JSON data from the request body
+    test = body.get("testid")
+    restest = client.table("testresult_duplicate").select("*").eq("testid", test).execute()
+    if restest.data:
+        content=restest.data[0]
+        result = {}
+        envi = content["envi"]
+        if envi ==0:
+            result["setting"] = "snort2"
+        if envi ==1:
+            result["setting"] = "snor3"
+        if envi ==2:
+            result["setting"] = "suricata"
+        result["text"] = content["rule"]
+        result["cve"] = content["CVEnum"]
+        result["created_at"] = content["created_at"]
+        result["attacklatency"] = float(content['attacktiemafter']) - float(content['attacktiembefore'])
+        result["normallatency"] = float(content['normaltimeafter']) - float(content['normaltimebefore'])
+        result["total"] = content['normalnum'] + content['attacknum']
+        result["attacknum"] = content['attacknum'] + content['attackfalse']
+        result["normalnum"] = content['normalnum'] + content['normalfalse']
+        result["accuracy"] = (content['normalnum']+content['attacknum'])/(content['normalnum'] + content['attacknum'])
+        result["attackrate"] = content['attackfalse']/(content['attacknum'])
+        result["normalrate"] = content['normalfalse']/(content['normalnum'])
+        result["attacktrue"] = (content['attacknum']-content['attackfalse'])/(content['attacknum'])
+        result["normaltrue"] = (content['normalnum']-content['normalfalse'])/(content['normalnum'])
+        result["envi"] = envi
+        return JSONResponse(result)
+    else:
+        return JSONResponse(content={"error": "No data found"}, status_code=404)
+
 
 
 @router.post("/test/accept")
@@ -134,13 +185,13 @@ async def test(request:Request,response: Response,client=Depends(get_supabase_cl
     test = request.cookies.get("test")
     user = request.cookies.get("user")
     user_info = client.auth.get_user(user)
-    user_id = user_info["user"]["user"]["id"]
-    restest = client.table("testresult").select("*").is_("testid", test).execute()
+    user_id = user_info.user.id
+    restest = client.table("testresult").select("*").eq("testid", test).execute()
     res = (
-    client.table("testresult")
+    client.table("testresult_duplicate")
     .insert({"testid": test, "CVEnum": restest["data"][0]["CVEnum"],"userid": user_id})
     .execute()
 )
+    res 
     response.set_cookie(key="previd", value=res.data[0]['previd'])
     return {"status": "success"}
-'''
