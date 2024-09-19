24fd2a92178d93abafb30fc0ea1feb0981c17f16
jhnoh1 <141809365+jhnoh1@users.noreply.github.com>
2024-09-19 22:13:35 +0900

    Update mypage_routes.py

--- src/mypage/mypage_routes.py
+++ src/mypage/mypage_routes.py
@@ -14,15 +14,36 @@ async def home(request: Request):
     return templates.TemplateResponse("my_info.html", {"request": request})
 
 
-@router.get("/gptkey")
-async def gpt_key(request: Request,client=Depends(get_supabase_client)):
-    data = await request.json()
-    key =data.get("key")
-    chage_gpt(client=client,request=request,key=key)
+@router.get("/gptkey", response_class=HTMLResponse)
+async def gpt_key(request: Request):
     return templates.TemplateResponse("chage_gptkey.html", {"request": request})
 
 @router.get("/pastresult", response_class=HTMLResponse)
 async def past(request: Request,client=Depends(get_supabase_client)):
     final = past_final(client=client,request=request)
+    return templates.TemplateResponse("past_result.html", {"data": final,"request": request})
+
+@router.get("/ruleresult", response_class=HTMLResponse)
+async def past(request: Request,client=Depends(get_supabase_client)):
     test = past_test(client=client,request=request)
-    return templates.TemplateResponse("past_result.html", {"final": final,"test" : test})
+    return templates.TemplateResponse("my_ruletest.html", {"data" : test,"request": request})
+
+@router.get("/ruleshow", response_class=HTMLResponse)
+async def past(request: Request,testid,client=Depends(get_supabase_client)):
+    test_result = client.table("test_result").select("*").eq("id", testid).execute()
+    return templates.TemplateResponse("my_ruletest_show.html", {"test_result": test_result,"request": request})
+
+
+@router.post("/gptkey/change")
+async def gpt_key(request: Request,client=Depends(get_supabase_client)):
+    data = await request.json()
+    key =data.get("key")
+    chage_gpt(client=client,request=request,key=key)
+    return "success"
+
+@router.post("/card/change")
+async def gpt_card(request: Request,client=Depends(get_supabase_client)):
+    data = await request.json()
+    key =data.get("card")
+    chage_card(client=client,request=request,key=key)
+    return "success"
