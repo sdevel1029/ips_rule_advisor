eaf14161027d8590c09d95e1897de9138dc49b0a
jhnoh1 <141809365+jhnoh1@users.noreply.github.com>
2024-09-21 23:25:27 +0900

    Update mypage_service.py

--- src/mypage/mypage_service.py
+++ src/mypage/mypage_service.py
@@ -1,3 +1,4 @@
+
 from supabase import Client
 from fastapi import HTTPException, Response, Request
 from fastapi.responses import RedirectResponse
@@ -41,8 +42,8 @@ def past_info(client: Client, request:Request):
         user = request.cookies.get("user")
         user_info = client.auth.get_user(user)
         user_id = user_info.user.id
-        past_test = client.table("info").select("*").eq("user_id", user_id).execute()
-        return past_test.data
+        past_info = client.table("info").select("*").eq("user_id", user_id).execute()
+        return past_info.data
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
