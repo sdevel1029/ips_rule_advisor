3c9a397ba1df3a929c5c820e9010c661bdf9d404
DESKTOP-1KU4MK0\gkok1 <sdevel1029@gmail.com>
2024-09-27 02:43:53 +0900

    룰 테스트 목록 에서 테스트 결과로 이동 가능하게 수정

--- src/sidebar/sidebar_service.py
+++ src/sidebar/sidebar_service.py
@@ -30,7 +30,7 @@ async def get_past_test_list(client: Client, request: Request, response: Respons
     user_id = user_info["user"].user.id
     
     # 해당 사용자의 테스트 리스트 가져오기
-    response = client.table("test_result").select("cve").eq("user_id", user_id).execute()
+    response = client.table("test_result").select("id, cve").eq("user_id", user_id).execute()
     
     # 데이터가 없는 경우에 대한 처리
     if not response.data:
