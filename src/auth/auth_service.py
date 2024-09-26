196e8c02d0dfd0f4dff5956ba80f42f998988fa1
jhnoh1 <141809365+jhnoh1@users.noreply.github.com>
2024-09-26 21:58:16 +0900

    Update auth_service.py

--- src/auth/auth_service.py
+++ src/auth/auth_service.py
@@ -29,6 +29,7 @@ def sign_in(client: Client, email: str, password: str, response: Response):
         res = client.auth.sign_in_with_password({"email": email, "password": password})
         if res.user:
             response.set_cookie(key="user", value=res.session.access_token)
+            response.set_cookie(key="userrefresh", value=res.session.refresh_token)
             return {"status": "success"}
         else:
             raise HTTPException(status_code=400, detail="Invalid credentials")
@@ -39,6 +40,7 @@ def sign_in(client: Client, email: str, password: str, response: Response):
 def sign_out(client: Client, response: Response):
     try:
         response.delete_cookie(key="user")
+        response.delete_cookie(key="userrefresh")
         client.auth.sign_out()
         return {"status": "success"}
     except Exception as e:
@@ -59,10 +61,39 @@ def callback(client: Client, request: Request, response: Response):
         rcode = request.query_params.get("refresh_token")
         response = RedirectResponse(url="/")
         response.set_cookie(key="user", value=code)
-        client.auth.set_session(code, rcode)
+        response.set_cookie(key="userrefresh", value=rcode)
+        res = client.auth.set_session(code, rcode)
+        print(res.session.access_token)
         return response  # 어디로 보낼지는 체크필요
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
+    
+
+
+def getuserinfo(client: Client, request: Request, response: Response):
+    try:
+        try:
+            # 쿠키에서 세션 토큰 가져오기
+            session_token_from_cookie = request.cookies.get("user")
+            refresh_token_from_cookie = request.cookies.get("userrefresh")
+            if not session_token_from_cookie:
+                return {"status":False}
+
+            # 세션 토큰을 사용하여 사용자 정보 가져오기
+            user_info = client.auth.get_user(session_token_from_cookie)
+            if user_info:
+                return {"status":True, "user": user_info}
+        except Exception as e:
+            res = client.auth.refresh_session(refresh_token=refresh_token_from_cookie)
+            response.set_cookie(key="user", value=res.session.access_token)
+            response.set_cookie(key="userrefresh", value=res.session.refresh_token)
+            user_info = client.auth.get_user(res.session.access_token)
+            if user_info:
+                return {"status":True, "user": user_info}
+    except Exception as e:
+        response.delete_cookie(key="user")
+        response.delete_cookie(key="userrefresh")
+        return {"status":False}
 
 
 def profile(client: Client, request: Request):
@@ -80,8 +111,3 @@ def profile(client: Client, request: Request):
             raise HTTPException(status_code=401, detail="Invalid session token")
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
-'''
-def mypage(client: Client, request: Request):
-    try:
-    except
-    '''
