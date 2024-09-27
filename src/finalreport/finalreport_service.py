ed53c2bb340f1b6099efd75d03b32f75da4d13b1
DESKTOP-1KU4MK0\gkok1 <sdevel1029@gmail.com>
2024-09-28 06:38:51 +0900

    최종보고서 생성기능 추가 ( 임시 )

--- src/finalreport/finalreport_service.py
+++ src/finalreport/finalreport_service.py
@@ -1,64 +1,36 @@
 from src.database.supabase_client import get_supabase_client
-from src.auth.auth_service import profile  
+from src.auth.auth_service import getuserinfo
 from src.database.comment_data import *
-from fastapi import Request
+from src.openai.openai_service import generate_report
+from fastapi import Request, Response
 
 supabase = get_supabase_client()
 
-def get_final_report(request: Request, cve_code: str):
-    # 쿠키에서 사용자 세션을 통해 사용자 정보 가져오기
-    user_profile = profile(supabase, request)
-    user_id = user_profile['user'].user.id
+async def get_final_report(request: Request, response: Response, infoid: str, testid: str, client, ):
+    user_info = getuserinfo(client=client, response=response, request=request)
+    user_id = user_info["user"].user.id
 
-    # Supabase에서 사용자와 CVE 코드가 일치하는 정보 조회
+    # Supabase에서 정보 수집 결과 가져오기
     info_result = supabase \
         .from_("info") \
         .select("*") \
+        .eq("id", infoid) \
         .eq("user_id", user_id) \
-        .eq("cve", cve_code) \
         .execute()
-    
-    # Supabase에서 사용자와 CVE 코드가 일치하는 테스트 결과 조회
+
+    # Supabase에서 테스트 결과 가져오기
     test_result = supabase \
         .from_("test_result") \
         .select("*") \
+        .eq("id", testid) \
         .eq("user_id", user_id) \
-        .eq("cve", cve_code) \
         .execute()
     
-    # Supabase에서 댓글 데이터 조회
-    comment_result = supabase \
-        .from_("comments") \
-        .select("*") \
-        .eq("cve", cve_code) \
-        .execute()
+    # OpenAI GPT 보고서 생성 함수 호출
+    report_content = await generate_report(info_result.data, test_result.data)
+    return report_content
 
-    # Supabase 쿼리 결과에서 실제 데이터 추출
-    results = {
-        "info": info_result.data,
-        "test": test_result.data,
-        "comments": comment_result.data if comment_result.data else []
-    }
+
     
-    return results
+    
 
-async def add_comment(request: Request, comment: Comment) -> Comment:
-    user_profile = profile(supabase, request)
-    user_id = user_profile['user'].user.id
-
-    print(user_id)
-    # 사용자 ID를 comment 객체에 추가
-    comment_data = comment.model_dump()
-    comment_data['user_id'] = user_id
-
-    print(f"Comment data to be inserted: {comment_data}")
-
-    response = supabase.table("comments").insert(comment_data).execute()
-
-    # 응답의 data 속성 확인
-    if not response.data:
-        raise Exception("Failed to add comment: No data returned")
-
-    print(f"Response data: {response.data}")
-
-    return Comment(**response.data[0])
