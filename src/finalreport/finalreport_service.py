b9628c4de8e5c03a7441ad7b29780c70b0d9368b
DESKTOP-1KU4MK0\gkok1 <sdevel1029@gmail.com>
2024-10-01 06:36:38 +0900

    최종보고서 페이지 추가 (임시 v2)

--- src/finalreport/finalreport_service.py
+++ src/finalreport/finalreport_service.py
@@ -30,6 +30,35 @@ async def get_final_report(request: Request, response: Response, infoid: str, te
     report_content = await generate_report(info_result.data, test_result.data)
     return report_content
 
+async def get_final_info(request: Request, response: Response, infoid: str, testid: str, client, ):
+    user_info = getuserinfo(client=client, response=response, request=request)
+    user_id = user_info["user"].user.id
+
+    # Supabase에서 정보 수집 결과 가져오기
+    info_result = supabase \
+        .from_("info") \
+        .select("*") \
+        .eq("id", infoid) \
+        .eq("user_id", user_id) \
+        .execute()
+
+    # Supabase에서 테스트 결과 가져오기
+    test_result = supabase \
+        .from_("test_result") \
+        .select("*") \
+        .eq("id", testid) \
+        .eq("user_id", user_id) \
+        .execute()
+    
+    info_data = info_result.data[0] if info_result.data else None
+    test_data = test_result.data[0] if test_result.data else None
+
+    return {
+            "info": info_data,
+            "test": test_data
+        }
+
+
 
     
     
