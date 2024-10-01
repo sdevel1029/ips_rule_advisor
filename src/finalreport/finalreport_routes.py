19455b03a0097462cce96b75e4354d2a8e611f9e
DESKTOP-1KU4MK0\gkok1 <sdevel1029@gmail.com>
2024-10-02 03:49:45 +0900

    최종보고서 저장및 생성 로직 변경 + 사이드바에서  최종보고서 결과 리스트 추가 및 확인 기능

--- src/finalreport/finalreport_routes.py
+++ src/finalreport/finalreport_routes.py
@@ -1,6 +1,6 @@
 # src/getinfo/finalreport_routes.py
-from fastapi import APIRouter, Request, HTTPException, Depends
-from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi import APIRouter, Request, Depends
+from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from src.finalreport.finalreport_service import *
 from src.database.comment_data import *
@@ -15,10 +15,28 @@ templates = Jinja2Templates(directory="src/templates/")
 async def final_report_page(request: Request):
     return templates.TemplateResponse("finalreport.html", {"request": request})
 
+@router.get("/finalcreate", response_class=HTMLResponse)
+async def final_report_create(request: Request, response: Response, infoid, testid, client=Depends(get_supabase_client)):
+    report_id, report_content = await get_final_create(request, response, infoid, testid, client)
+    result = await get_final_info(request, response, infoid, testid, report_id, client)
+    return templates.TemplateResponse("final_show.html", {"request": request, "report_content": report_content, "data":result, "report_id": report_id})
+
 @router.get("/finalshow", response_class=HTMLResponse)
-async def final_report_show(request: Request, response: Response, infoid, testid, client=Depends(get_supabase_client)):
-    report_content = await get_final_report(request, response, infoid, testid, client)
-    result = await get_final_info(request, response, infoid, testid, client)
-    return templates.TemplateResponse("final_show.html", {"request": request, "report_content": report_content, "data":result})
+async def final_report_create(request: Request, response: Response, report_id, client=Depends(get_supabase_client)):
+    final_ids = await get_final_info_ids(request, response, report_id, client)
 
+    if final_ids is None:
+        return HTMLResponse(content="Final report not found", status_code=404)
 
+    infoid = final_ids["infoid"]
+    testid = final_ids["testid"]
+    
+    result = await get_final_info(request, response, infoid, testid, report_id, client)
+    report_content = result['final']['content']
+    return templates.TemplateResponse("final_show.html", {"request": request, "report_content": report_content, "data":result, "report_id": report_id})
+
+@router.post("/comments", response_model=Comment)
+async def create_comment(request: Request, comment: Comment, client=Depends(get_supabase_client)):
+    comment = await add_comment(request, comment, client)
+    print("test" , comment) 
+    return comment
