49fdefb25f1fdee60670b49034c8fcdb8dbb6bce
DESKTOP-1KU4MK0\gkok1 <sdevel1029@gmail.com>
2024-10-20 22:56:12 +0900

    정보검색 최적화 + user_id (비로그인시) 오류 메세지 출력 수정

--- src/sidebar/sidebar_service.py
+++ src/sidebar/sidebar_service.py
@@ -1,13 +1,14 @@
 from supabase import Client
 from fastapi import HTTPException, Request, Response
-from fastapi.responses import RedirectResponse
 from src.auth.auth_service import getuserinfo
 
 async def get_past_cve_list(client: Client, request: Request, response: Response):
     # 사용자 정보 가져오기
-    user_info = getuserinfo(client=client,response=response,request=request)
-    if isinstance(user_info, RedirectResponse):
-        return user_info
+    user_info = getuserinfo(client=client, response=response, request=request)
+    
+    if not user_info or "user" not in user_info:
+        raise HTTPException(status_code=401)
+    
     user_id = user_info["user"].user.id
     
     # 해당 사용자의 CVE 리스트, 결과물의 id 값 가져오기
@@ -21,9 +22,11 @@ async def get_past_cve_list(client: Client, request: Request, response: Response
 
 async def get_past_test_list(client: Client, request: Request, response: Response):
     # 사용자 정보 가져오기
-    user_info = getuserinfo(client=client,response=response,request=request)
-    if isinstance(user_info, RedirectResponse):
-        return user_info
+    user_info = getuserinfo(client=client, response=response, request=request)
+    
+    if not user_info or "user" not in user_info:
+        raise HTTPException(status_code=401)
+    
     user_id = user_info["user"].user.id
     
     # 해당 사용자의 테스트 리스트 가져오기
@@ -37,9 +40,11 @@ async def get_past_test_list(client: Client, request: Request, response: Respons
 
 async def get_final_report_list(client: Client, request: Request, response: Response):
     # 사용자 정보 가져오기
-    user_info = getuserinfo(client=client,response=response,request=request)
-    if isinstance(user_info, RedirectResponse):
-        return user_info
+    user_info = getuserinfo(client=client, response=response, request=request)
+    
+    if not user_info or "user" not in user_info:
+        raise HTTPException(status_code=401)
+    
     user_id = user_info["user"].user.id
     
     # 해당 사용자의 테스트 리스트 가져오기
